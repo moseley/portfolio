bdba54b20b45041eec20ffbee73d7a5de3a38d25
Jeremy Moseley <jeremymoseley@me.com>
2021-01-28 08:34:09 -0800

    Remove title param

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,8 @@
 import { Flex, Heading, Text } from '@chakra-ui/react'
 
-export const Hero = ({ title }: { title: string }) => (
+export const Hero = () => (
   <Flex justifyContent='center' alignItems='center' height='100vh'>
     <Heading fontSize='6vw'>Jeremy Moseley</Heading>
     <Text fontSize='lg'>Bay area based full stack developer</Text>
   </Flex>
 )
-
-Hero.defaultProps = {
-  title: 'Jeremy Moseley'
-}
